72bf13f6cc6f9f66011ad7d49d11384d8a8b4d54
Kunal Kushwaha <151702898+KunalKushwaha1806@users.noreply.github.com>
2026-07-23 10:29:26 +0530

    Time: 3 ms (70.88%), Space: 20.6 MB (44.97%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i=1
+        while i<len(nums):
+            if nums[i]!=nums[i-1]:
+                return nums[i-1]
+            i+=3
+        return nums[-1]
+        
