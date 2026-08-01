e9f35fd82dec5c6e1f46dce297a3cfe03528ad16
Kunal Kushwaha <151702898+KunalKushwaha1806@users.noreply.github.com>
2026-08-01 12:04:13 +0530

    Time: 3 ms (59.15%), Space: 21.4 MB (17.35%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,17 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        @cache
+        memo={}
         def solve(i,flag):
             if i>=n:
                 return 0
+            if (i,flag) in memo:
+                return memo[(i,flag)]
             if flag:
                 sell=prices[i]+solve(i+2,0)
                 not_sell=solve(i+1,1)
-                return max(sell,not_sell)
+                memo[(i,flag)]=max(sell,not_sell)
             else:
                 buy=-prices[i]+solve(i+1,1)
                 not_buy=solve(i+1,0)
-                return max(buy,not_buy)
+                memo[(i,flag)]=max(buy,not_buy)
+            return memo[(i,flag)]
         return solve(0,0)
         
