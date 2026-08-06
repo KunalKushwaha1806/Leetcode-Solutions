61bad96b2230846d00867ccfc3e94ddc64bc6c2d
Kunal Kushwaha <151702898+KunalKushwaha1806@users.noreply.github.com>
2026-08-06 15:08:56 +0530

    Time: 451 ms (45.58%), Space: 52.4 MB (46.01%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,21 +1,17 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n=len(coins)
-        memo=[[-1]*(amount+1) for _ in range(n+1)]
-        def s(i,tar):
-            if memo[i][tar]!=-1:
-                return memo[i][tar]
-            if tar==0:
-                return 1
-            if i==n:
-                return 0
-            take=0
-            if tar>=coins[i]:
-                take=s(i,tar-coins[i])
-            not_take=s(i+1,tar)
-            memo[i][tar]=take+not_take
-           
-            return memo[i][tar]
-        return s(0,amount)
+        memo=[[0]*(amount+1) for _ in range(n+1)]
+        for i in range(n):
+            memo[i][0]=1
+        for i in range(n-1,-1,-1):
+            for j in range(1,amount+1):
+                take=0
+                if coins[i]<=j:
+                    take=memo[i][j-coins[i]]
+                not_take=memo[i+1][j]
+                memo[i][j]=take+not_take
+            
+        return memo[0][amount]
 
         
