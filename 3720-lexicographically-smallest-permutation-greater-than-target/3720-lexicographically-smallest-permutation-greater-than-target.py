81efbfc3fbbcca0cf2d3faa50e65446f270463d2
Kunal Kushwaha <151702898+KunalKushwaha1806@users.noreply.github.com>
2026-08-27 21:47:47 +0530

    Time: 8 ms (71.05%), Space: 19.4 MB (71.05%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -4,12 +4,10 @@ class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
         count = Counter(s)
         n = len(s)
-        
         matched = 0
         while matched < n and count[target[matched]] > 0:
             count[target[matched]] -= 1
             matched += 1
-            
         for i in range(matched, -1, -1):
             if i < n:
                 bigger = [c for c in count if c > target[i] and count[c] > 0]
@@ -18,8 +16,6 @@ class Solution:
                     count[c] -= 1
                     rest = ''.join(ch * count[ch] for ch in sorted(count))
                     return target[:i] + c + rest
-            
             if i > 0:
-                count[target[i - 1]] += 1
-                
+                count[target[i - 1]] += 1 
         return ""
