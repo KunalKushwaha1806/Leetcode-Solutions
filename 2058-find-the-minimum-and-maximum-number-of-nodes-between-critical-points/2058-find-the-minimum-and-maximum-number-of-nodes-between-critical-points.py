80c44c620fbc3390e4e4c76c2cc563d25791d544
Kunal Kushwaha <151702898+KunalKushwaha1806@users.noreply.github.com>
2026-08-31 14:22:59 +0530

    Time: 121 ms (8.56%), Space: 62.3 MB (98.99%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -25,12 +25,10 @@ class Solution:
                 c_p.append(i)
         if len(c_p) < 2:
             return [-1, -1]
-        min_dist = float("inf")
-        for i in range(1, len(c_p)):
-            min_dist = min(min_dist, c_p[i] - c_p[i - 1])
-
-        max_dist = c_p[-1] - c_p[0]
-
-        return [min_dist, max_dist]
+        min_dist=float("inf")
+        for i in range(1,len(c_p)):
+            min_dist=min(min_dist,c_p[i]-c_p[i-1])
+        max_dist=c_p[-1]-c_p[0]
+        return [min_dist,max_dist]
 
         
