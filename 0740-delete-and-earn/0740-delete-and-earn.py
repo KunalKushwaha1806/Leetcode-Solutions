ac024a1096f4d1f1bb5709a5eb922a7f9d9bb197
Kunal Kushwaha <151702898+KunalKushwaha1806@users.noreply.github.com>
2026-09-03 14:58:07 +0530

    Time: 11 ms (31.31%), Space: 21.9 MB (21.75%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,26 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        if not nums:
+            return
+        pos={}
+        for i in range(len(nums)):
+            if nums[i] not in pos:
+                pos[nums[i]]=[i]
+            else:
+                pos[nums[i]].append(i)
+        unique_n=sorted(pos.keys())
+        take = 0
+        skip = 0
+
+        for i in range(len(unique_n)):
+            val = unique_n[i]
+            points = val * len(pos[val])
+
+            if i > 0 and unique_n[i - 1] == val - 1:
+                new_take = skip + points
+            else:
+                new_take = max(take, skip) + points
+            new_skip = max(take, skip)
+            take, skip = new_take, new_skip
+
+        return max(take, skip)
