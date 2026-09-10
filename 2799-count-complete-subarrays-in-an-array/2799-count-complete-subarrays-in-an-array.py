73ff094cc6651254d2e0b5fcc582c45208976244
Kunal Kushwaha <151702898+KunalKushwaha1806@users.noreply.github.com>
2026-09-10 14:03:12 +0530

    Time: 548 ms (20.48%), Space: 19.5 MB (14.02%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -1,13 +1,15 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        target_distinct = len(set(nums))
-        ans = 0
-        n = len(nums)
-        for i in range(n):
-            current_elements = set()
-            for j in range(i, n):
-                current_elements.add(nums[j])
-                if len(current_elements) == target_distinct:
-                    ans += 1
 
-        return ans
+        ans=0
+        distinct_e=len(set(nums))
+        n=len(nums)
+
+        for i in range(n):
+            curr_e=set()
+            for j in range(i,n):
+                curr_e.add(nums[j])
+                if len(curr_e)==distinct_e:
+                    ans+=1
+        return ans 
+        
